6640a6ffc9c195f59859c9ec65464e04cf2ed870
Roxter8 <143808912+Roxter8@users.noreply.github.com>
2025-12-24 21:14:36 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
 
 createRoot(root).render(
-  <TonConnectUIProvider manifestUrl="https://mvp-quicky.vercel.app/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://mvp-quicky2-xv2b.vercel.app/tonconnect-manifest.json">
     <App />
   </TonConnectUIProvider>
 );
